0da55ff2f2be82c060024fd0ec629281c8359d4c
jaydev-code <jayfelisdev@gmail.com>
2026-01-23 21:52:00 +0800

    fix: extract pure hash with cut for accurate comparison

--- file-integrity-checker.sh
+++ file-integrity-checker.sh
@@ -6,8 +6,24 @@ file_info_hash="file_info_hash.txt"
 
 if [[ ! -f "$file_info_hash" ]]; then
    echo "saving fingerprint of $file"
-   md5sum "$file" > "$file_info_hash"
+   md5sum "$file" | cut -d' ' -f1 > "$file_info_hash"
    echo "done.."
    exit 0
 
 fi 
+
+
+echo "second run to check and confirm"
+
+old_save_hash=$(cat "$file_info_hash")
+save_new_hash=$( md5sum "$file" | cut -d' ' -f1 )
+
+if [[ "$old_save_hash" = "$save_new_hash" ]] ; then
+
+  echo "nothing changes to your $file" 
+
+else
+
+   echo "warning !! something changed to your $file"
+
+fi
