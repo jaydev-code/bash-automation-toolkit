5b4bfa60a68c80d79ff139521b6cadd5160d404a
jaydev-code <jayfelisdev@gmail.com>
2026-02-23 19:12:38 +0800

    fix: text formating and readability

--- network/bandwidth-monitor.sh
+++ network/bandwidth-monitor.sh
@@ -2,28 +2,31 @@
 
 
 
+
+
 if [ -d /sys/class/net/wlan0 ] ; then
  
  NETWORK="wlan0"
- echo "mMy network is $NETWORK"
+ echo "My network is $NETWORK"
 
 elif [ -d /sys/class/net/eth0 ] ; then 
  
  NETWORK="eth0"
- echo "my network is $NETWORK"
+ echo "My network is $NETWORK"
 
 else 
 
  echo "sorry, cant find network"
+ exit 1
 
 fi
 
-first_recieved=$(cat /sys/class/net/$NETWORK/statistics/rx_bytes)
+first_received=$(cat /sys/class/net/$NETWORK/statistics/rx_bytes)
 sleep 3 
-second_recieved=$(cat /sys/class/net/$NETWORK/statistics/rx_bytes)
+second_received=$(cat /sys/class/net/$NETWORK/statistics/rx_bytes)
 
-f=$(( ($second_recieved - $first_recieved) / 3 ))
+bytes_per_sec=$(( ($second_received - $first_received) / 3 ))
 
-d=$(( $f / 1000 ))
+kb_per_sec=$(( $bytes_per_sec / 1000 ))
 
-echo "$d kB/s"
+echo "$kb_per_sec kB/s"
